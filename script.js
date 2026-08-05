f76fe8ab9783ab0e1bf65bb6725c0c817e67d197
JuiceLoc <alejo120821@gmail.com>
2026-08-05 11:28:57 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ window.addEventListener('load', () => {
     ScrollTrigger.matchMedia({
         "(min-width: 768px)": function() {
             gsap.to('.hero-screen', {
-                scale: 0.7,             // Reducción suave hacia el fondo
-                opacity: 0,             // Desvanecimiento gradual
-                filter: 'blur(12px)',   // Desenfoque sutil
+                scale: 0.7,
+                opacity: 0,
+                filter: 'blur(12px)',
                 ease: 'none',
                 scrollTrigger: {
                     trigger: '.hero-pinned-wrapper',
                     start: 'top top',
                     end: 'bottom top',
-                    scrub: true,        // Sincronización exacta y suave con el scroll
+                    scrub: true,
                     pin: true,
                     pinSpacing: false
                 }
